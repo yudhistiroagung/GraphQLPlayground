fbe5385f758e9d9e7ab01ed99fe6427328ee0065
Yudhistiro Agung <yudhistiroagung@btpnmacs-MacBook-Pro.local>
2020-01-10 11:06:43 +0700

    refactor add user container

--- app/App.tsx
+++ app/App.tsx
@@ -11,27 +11,10 @@
 import React, { useState } from 'react';
 import {
     StyleSheet,
-    View,
-    ActivityIndicator,
     SafeAreaView,
-    Button,
-    TextInput,
 } from 'react-native';
 
-import { Mutation } from 'react-apollo';
-import { gql } from 'apollo-boost'
-
-import { User } from './models/User';
-
-import { UserListContainer } from './containers'
-
-const MUTATION_ADD_USER = gql`mutation addUser($user: CreateUserInput!) {
-    createUser(input: $user) {
-        id
-        name
-        email
-    }
-}`
+import { UserListContainer, AddUser } from './containers'
 
 const LIST = 'LIST'
 const ADD_EDIT = 'ADD_EDIT'
@@ -39,7 +22,6 @@ const ADD_EDIT = 'ADD_EDIT'
 const App = () => {
 
     const [screen, setScreen] = useState(LIST)
-    const [user, setUser] = useState(undefined)
 
     const toAddUser = () => {
         setScreen(ADD_EDIT)
@@ -51,7 +33,7 @@ const App = () => {
 
     const componentToBeRendered = screen === LIST
         ? <UserListContainer toAddUser={toAddUser} />
-        : <AddEditScreen user={user} cancel={toList} />
+        : <AddUser cancel={toList} />
 
     return (
         <SafeAreaView style={styles.container}>
@@ -60,80 +42,6 @@ const App = () => {
     );
 };
 
-const renderLoadingIndicator = () => {
-    return <ActivityIndicator size={"large"} color="green" />;
-}
-
-// Add edit screen
-interface AddEditScreenProps {
-    user?: User;
-    cancel: () => void;
-}
-const AddEditScreen = (props: AddEditScreenProps) => {
-    const { user, cancel } = props;
-
-    const [name, setName] = useState(user ? user.name : '')
-    const [username, setUsername] = useState(user ? user.username : '')
-    const [email, setEmail] = useState(user ? user.email : '')
-
-    return (
-        <Mutation mutation={MUTATION_ADD_USER}>
-            {(addUser: any, { loading, error, data }: any) => {
-                console.log('error', error);
-                if (!!data) cancel()
-                if (loading) return renderLoadingIndicator()
-                return (
-                    <View style={styles.screen}>
-                        <Button
-                            title="Back"
-                            onPress={cancel}
-                        />
-                        <TextInput
-                            style={styles.inputText}
-                            value={name}
-                            autoCorrect={false}
-                            underlineColorAndroid="transparent"
-                            placeholder="Input user name"
-                            onChangeText={(value: string) => setName(value)}
-                        />
-                        <TextInput
-                            style={styles.inputText}
-                            value={username}
-                            autoCorrect={false}
-                            underlineColorAndroid="transparent"
-                            placeholder="Input user username"
-                            onChangeText={(value: string) => setUsername(value)}
-                        />
-                        <TextInput
-                            style={styles.inputText}
-                            value={email}
-                            autoCorrect={false}
-                            underlineColorAndroid="transparent"
-                            placeholder="Input user email"
-                            onChangeText={(value: string) => setEmail(value)}
-                        />
-                        <Button
-                            onPress={() => {
-                                const payload = {
-                                    variables: {
-                                        user: {
-                                            name,
-                                            username,
-                                            email
-                                        },
-                                    }
-                                };
-                                addUser(payload)
-                            }}
-                            title="Add"
-                        />
-                    </View>
-                )
-            }}
-        </Mutation>
-    )
-}
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -141,23 +49,6 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: '#ffffff'
     },
-    screen: {
-        flex: 1,
-        width: '100%',
-    },
-    buttonAdd: {
-        height: 48,
-        backgroundColor: 'blue',
-        margin: 8
-    },
-    inputText: {
-        height: 40,
-        margin: 8,
-        paddingLeft: 4,
-        borderWidth: 1,
-        borderColor: '#e6e6e6',
-        borderRadius: 4
-    }
 });
 
 export default App;
---
+++ app/containers/AddUser/AddUser.container.tsx
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+
+import {
+    View,
+    Button,
+    TextInput,
+    ActivityIndicator,
+} from 'react-native';
+
+import { gql } from 'apollo-boost';
+import { Mutation } from 'react-apollo';
+
+import styles from './AddUser.styles'
+
+const MUTATION_ADD_USER = gql`mutation addUser($user: CreateUserInput!) {
+    createUser(input: $user) {
+        id
+        name
+        email
+    }
+}`
+
+interface AddEditScreenProps {
+    cancel: () => void;
+}
+export const AddUser = (props: AddEditScreenProps) => {
+    const { cancel } = props;
+
+    const [name, setName] = useState('')
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+
+    const renderLoading = () => {
+        return <ActivityIndicator size={"large"} color="green" />;
+    }
+
+    return (
+        <Mutation mutation={MUTATION_ADD_USER}>
+            {(addUser: any, { loading, error, data }: any) => {
+                console.log('error', error);
+                if (!!data) cancel()
+                if (loading) return renderLoading()
+                return (
+                    <View style={styles.container}>
+                        <Button
+                            title="Back"
+                            onPress={cancel}
+                        />
+                        <TextInput
+                            style={styles.inputText}
+                            value={name}
+                            autoCorrect={false}
+                            underlineColorAndroid="transparent"
+                            placeholder="Input user name"
+                            onChangeText={(value: string) => setName(value)}
+                        />
+                        <TextInput
+                            style={styles.inputText}
+                            value={username}
+                            autoCorrect={false}
+                            underlineColorAndroid="transparent"
+                            placeholder="Input user username"
+                            onChangeText={(value: string) => setUsername(value)}
+                        />
+                        <TextInput
+                            style={styles.inputText}
+                            value={email}
+                            autoCorrect={false}
+                            underlineColorAndroid="transparent"
+                            placeholder="Input user email"
+                            onChangeText={(value: string) => setEmail(value)}
+                        />
+                        <Button
+                            onPress={() => {
+                                const payload = {
+                                    variables: {
+                                        user: {
+                                            name,
+                                            username,
+                                            email
+                                        },
+                                    }
+                                };
+                                addUser(payload)
+                            }}
+                            title="Add"
+                        />
+                    </View>
+                )
+            }}
+        </Mutation>
+    )
+}
